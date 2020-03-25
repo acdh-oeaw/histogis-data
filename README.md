6fabcda4e10df5436734b3c1339524d92881459d
csae8092 <peter.andorfer@oeaw.ac.at>
2020-03-25 13:54:40 +0100

    data update; script to dump data added

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.2615387.svg)](https://doi.org/10.5281/zenodo.2615387)
 
 Data created by HistoGIS
+
+to get a fresh dump make sure you have `acdh-histogis` installed
+
+```shell
+pip install acdh-histogis
+```
+
+run `python dump_all.py`
---
+++ dump_all.py
@@ -0,0 +1,14 @@
+import os
+import glob
+import json
+from histogis.histogis import HistoGis
+from histogis.histogis import merge_single_files
+
+
+try:
+    os.mkdir('single_files')
+except FileExistsError:
+    pass
+
+# dump = HistoGis().dump_all_file_per_feature(path='single_files')
+all_in_one = merge_single_files()
