dabbaa2cf924851a00c3ce33f27feee1bcdaddb6
csae8092 <peter.andorfer@oeaw.ac.at>
2020-04-17 09:36:24 +0200

    wiki-id -> wiki-links, adde 'title' to properties; Reichsreise added

--- dump_all.py
+++ dump_all.py
@@ -10,5 +10,5 @@ try:
 except FileExistsError:
     pass
 
-# dump = HistoGis().dump_all_file_per_feature(path='single_files')
+dump = HistoGis().dump_all_file_per_feature(path='single_files')
 all_in_one = merge_single_files()
